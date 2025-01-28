a35a350602368e1b39ac07ef5201ff68a32ed94c
kc0588615 <kc0588615@students.mclennan.edu>
2025-01-27 21:44:41 -0600

    deleted duplicate eventBus and phasergame

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,5 +1,5 @@
 import { Scene } from 'phaser';
-import { EventBus } from '../../EventBus';
+import { EventBus } from '../EventBus';
 import GridController from '../core/GridController';
 import { IGem } from '../types/Gem';
 
